e68cccb110ce3e0547d2d951132ff204482e0ff5
AAVinogradov <aavinogradov@bk.ru>
2023-06-11 12:58:45 +0300

    добавил задачу по нахождению максимума из 9 чисел разными способами

--- Example005_ConditionIfElse/Program.cs
+++ Example005_ConditionIfElse/Program.cs
@@ -1,12 +1,11 @@
 ﻿Console.Write("Введите имя пользователя: ");
-string username = Console.ReadLine();
-
-if (username.ToLower() == "masha")
+string? UserName = Console.ReadLine();
+if (UserName.ToLower() == "masha")
 {
     Console.WriteLine("Ура, это же Маша!");
 }
 else
 {
     Console.Write("Привет, ");
-    Console.WriteLine(username);
+    Console.WriteLine(UserName);
 }

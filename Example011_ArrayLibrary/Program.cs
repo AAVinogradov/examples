b7b5a0f8dc432505ce1d585b32fb3f4c4e48ae3a
AAVinogradov <aavinogradov@bk.ru>
2023-06-11 14:44:18 +0300

    добавил задачу 11 заполнения массива случайными числами и вывод

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,28 @@
+﻿// заполнения массива случайными числами и вывод 
+
+//заполнение массива случайными числами
+void FillArray(int[] collection) // метод заполнения массива
+{
+    int lenght = collection.Length; // получение длмны массива
+    int index = 0;
+    while (index < lenght)
+    {
+        collection[index] = new Random().Next(1, 10); // положить в ячейку рандомное число от 1 до 10
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+int[] array = new int[10]; // создание массива равного 10 элементам, по умолчанию заполнен нулями
+
+FillArray(array);
+PrintArray(array);
